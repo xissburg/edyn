6b20e38d46cfc4dcd67df5ad1b77e994839969a9
xissburg <xissburg@xissburg.com>
2020-04-05 12:44:25 -0500

    Comments

--- include/edyn/comp/constraint_row.hpp
+++ include/edyn/comp/constraint_row.hpp
@@ -12,18 +12,30 @@ namespace edyn {
 struct constraint_row {
     std::array<entt::entity, max_relations> entity {make_array<max_relations>(entt::entity{entt::null})};
     std::array<bool, max_relations> use_spin {make_array<max_relations>(false)};
+
+    // Jacobian.
     std::array<vector3, max_relations * 2> J;
+    
     scalar error;
     scalar lower_limit;
     scalar upper_limit;
-    // Effective mass (J M^-1 J^T)^-1
+
+    // Effective mass (J M^-1 J^T)^-1.
     scalar eff_mass;
-    // Right hand side Jv + b
+
+    // Right hand side Jv + bias.
     scalar rhs;
+
+    // Error reduction parameter.
     scalar erp {0.2};
+
+    // Relative velocity at pivot points.
     scalar relvel;
     scalar restitution {0};
+
+    // Applied impulse.
     scalar impulse {0};
+
     int priority {0};
 };
 
