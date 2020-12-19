e36cb8bf07f82738759c2b2deb9ba9f2e716deb5
xissburg <xissburg@xissburg.com>
2020-12-19 15:26:59 -0500

    Update examples.

--- examples/current_pos/current_pos.cpp
+++ examples/current_pos/current_pos.cpp
@@ -7,9 +7,8 @@
  * Demonstration of the importance of interpolation for smooth real-time
  * presentation on screen using `edyn::present_position`. Using the raw
  * `edyn::position` will generally cause jitter. The value in 
- * `edyn::present_position` is delayed by `world.fixed_dt` with respect
- * to the current global time. It's calculated by translating the physics
- * position backwards using `linvel`.
+ * `edyn::present_position` is the `edyn::position` interpolated or 
+ * extrapolated by an amount of time using the `edyn::linvel` of the entity.
  */
 void print_entities(entt::registry& registry) {
     auto& world = registry.ctx<edyn::world>();
@@ -19,10 +18,8 @@ void print_entities(entt::registry& registry) {
     auto view = registry.view<const edyn::position, const edyn::present_position>();
     view.each([] (auto ent, const auto& pos, const auto& curpos) {
         // Compare the physics position to the presentation position and notice how
-        // the physics position `pos` does not change uniformly after each update 
-        // because the number of steps vary per update since the number of steps 
-        // taken is `floor(dt / fixed_dt)`, which could be zero sometimes as well. The
-        // presentation position `curpos` should change linearly with dt (because
+        // the physics position `pos` does not change uniformly after each update.
+        // The presentation position `curpos` should change linearly with dt (because
         // `linvel` is constant in this example).
         printf("pos    (%d): %.3f, %.3f, %.3f\n", entt::to_integral(ent), pos.x, pos.y, pos.z);
         printf("curpos (%d): %.3f, %.3f, %.3f\n", entt::to_integral(ent), curpos.x, curpos.y, curpos.z);
@@ -32,16 +29,15 @@ void print_entities(entt::registry& registry) {
 int main(int argc, char** argv) {
     edyn::init();
     entt::registry registry;
-
-    const auto ent = registry.create();
-    registry.emplace<edyn::dynamic_tag>(ent);
-    registry.emplace<edyn::position>(ent, 0, 0, 0);
-    registry.emplace<edyn::present_position>(ent);
-    // Set a constant speed that will move the entity 1 unit per step.
-    registry.emplace<edyn::linvel>(ent, 0, 1/0.041, 0);
-
     auto& world = registry.set<edyn::world>(registry);
     world.m_fixed_dt = 0.041;
+
+    auto def = edyn::rigidbody_def();
+    def.presentation = true;
+    def.mass = 10;
+    def.linvel = {0, 1/0.041, 0};
+    def.gravity = edyn::vector3_zero;
+    edyn::make_rigidbody(registry, def);
 
     for (;;) {
         world.update();
