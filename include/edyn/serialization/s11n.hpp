4ba1b29a45f8fbd2a4f9e45ac3b3eb3eef5c895d
xissburg <xissburg@xissburg.com>
2020-12-19 19:23:06 -0500

    Remove unused.

--- include/edyn/serialization/s11n.hpp
+++ include/edyn/serialization/s11n.hpp
@@ -7,6 +7,5 @@
 #include "edyn/serialization/triangle_mesh_s11n.hpp"
 #include "edyn/serialization/paged_triangle_mesh_s11n.hpp"
 #include "edyn/serialization/entt_s11n.hpp"
-#include "edyn/serialization/registry_s11n.hpp"
 #include "edyn/serialization/file_archive.hpp"
 #include "edyn/serialization/memory_archive.hpp"
