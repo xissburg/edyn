18b4d901ff418e782c1af67dbc3fa96ed8a1635f
xissburg <xissburg@xissburg.com>
2020-12-28 13:59:09 -0500

    Refactor plane shape.

--- include/edyn/shapes/plane_shape.hpp
+++ include/edyn/shapes/plane_shape.hpp
@@ -9,46 +9,36 @@ namespace edyn {
 struct plane_shape {
     vector3 normal;
     scalar constant;
-    static constexpr scalar aabb_size = 99999;
+    static constexpr scalar aabb_half_extent = 99999;
 
     AABB aabb(const vector3 &pos, const quaternion &orn) const {
+        vector3 unit_min, unit_max;
+
         if (normal == vector3_x) {
-            return {
-                {-aabb_size, -aabb_size, -aabb_size},
-                {         0,  aabb_size,  aabb_size}
-            };
+            unit_min = {-1, -1, -1};
+            unit_max = { 0,  1,  1};
+        } else if (normal == -vector3_x) {
+            unit_min = { 0, -1, -1};
+            unit_max = { 1,  1,  1};
+        } else if (normal == vector3_y) {
+            unit_min = {-1, -1, -1};
+            unit_max = { 1,  0,  1};
+        } else if (normal == -vector3_y) {
+            unit_min = {-1,  0, -1};
+            unit_max = { 1,  1,  1};
+        } else if (normal == vector3_z) {
+            unit_min = {-1, -1, -1};
+            unit_max = { 1,  1,  0};
+        } else if (normal == -vector3_z) {
+            unit_min = {-1, -1,  0};
+            unit_max = { 1,  1,  1};
+        } else {
+            unit_min = { 1 , 1 , 1};
+            unit_max = {-1, -1, -1};
         }
-        if (normal == -vector3_x) {
-            return {
-                {         0, -aabb_size, -aabb_size},
-                { aabb_size,  aabb_size,  aabb_size}
-            };
-        }
-        if (normal == vector3_y) {
-            return {
-                {-aabb_size, -aabb_size, -aabb_size},
-                { aabb_size,          0,  aabb_size}
-            };
-        }
-        if (normal == -vector3_y) {
-            return {
-                {-aabb_size,          0, -aabb_size},
-                { aabb_size,  aabb_size,  aabb_size}
-            };
-        }
-        if (normal == vector3_z) {
-            return {
-                {-aabb_size, -aabb_size, -aabb_size},
-                { aabb_size,  aabb_size,          0}
-            };
-        }
-        if (normal == -vector3_z) {
-            return {
-                {-aabb_size, -aabb_size,          0},
-                { aabb_size,  aabb_size,  aabb_size}
-            };
-        }
-        return {-vector3_max, vector3_max};
+        
+        return {unit_min * aabb_half_extent + pos, 
+                unit_max * aabb_half_extent + pos};
     }
 
     vector3 inertia(scalar mass) const {
