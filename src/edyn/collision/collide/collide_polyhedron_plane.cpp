31f28209d275e8a7a9605ea780a7fb6789affaa9
xissburg <xissburg@xissburg.com>
2021-04-08 23:27:18 -0500

    Increase support polygon tolerance for greater stability. Refactor polyhedron-plane collision.

--- src/edyn/collision/collide/collide_polyhedron_plane.cpp
+++ src/edyn/collision/collide/collide_polyhedron_plane.cpp
@@ -1,5 +1,6 @@
 #include "edyn/collision/collide.hpp"
 #include "edyn/collision/collision_result.hpp"
+#include "edyn/math/constants.hpp"
 #include "edyn/math/quaternion.hpp"
 #include "edyn/math/scalar.hpp"
 
@@ -15,35 +16,31 @@ collision_result collide(const polyhedron_shape &shA, const plane_shape &shB,
 
     auto normal = rotate(ornB, shB.normal);
     auto center = posB + rotate(ornB, shB.normal * shB.constant);
-    scalar min_distance = EDYN_SCALAR_MAX;
+    scalar distance = EDYN_SCALAR_MAX;
 
     // Find distance between polyhedron and plane.
     for (auto &rvertex : rmeshA.vertices) {
         auto vertex_world = posA + rvertex;
-        auto distance = dot(vertex_world - center, normal);
-
-        if (distance < min_distance) {
-            min_distance = distance;
-        }
+        auto vertex_dist = dot(vertex_world - center, normal);
+        distance = std::min(vertex_dist, distance);
     }
 
-    if (min_distance > ctx.threshold) return {};
+    if (distance > ctx.threshold) return {};
 
     // Add points to all vertices that are within a range from the
     // minimum distance.
-    const scalar distance_range = 0.002;
     auto result = collision_result{};
 
     for (size_t i = 0; i < rmeshA.vertices.size(); ++i) {
         auto vertex_world = posA + rmeshA.vertices[i];
-        auto distance = dot(vertex_world - center, normal);
+        auto vertex_dist = dot(vertex_world - center, normal);
 
-        if (distance > min_distance + distance_range) continue;
+        if (vertex_dist > distance + support_polygon_tolerance) continue;
 
         auto pivotA = shA.mesh->vertices[i];
-        auto pt_proj = vertex_world - normal * distance;
-        auto pivotB = to_object_space(pt_proj, posB, ornB);
-        result.maybe_add_point({pivotA, pivotB, shB.normal, distance});
+        auto pivotB_world = vertex_world - normal * vertex_dist;
+        auto pivotB = to_object_space(pivotB_world, posB, ornB);
+        result.maybe_add_point({pivotA, pivotB, shB.normal, vertex_dist});
     }
 
     return result;
