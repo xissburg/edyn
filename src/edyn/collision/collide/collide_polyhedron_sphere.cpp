8f24100f0699176853979a5ed489f7601ab20acd
xissburg <xissburg@xissburg.com>
2021-04-09 00:03:57 -0500

    Rename

--- src/edyn/collision/collide/collide_polyhedron_sphere.cpp
+++ src/edyn/collision/collide/collide_polyhedron_sphere.cpp
@@ -2,12 +2,12 @@
 
 namespace edyn {
 
-collision_result collide(const sphere_shape &shA, const polyhedron_shape &shB, 
+collision_result collide(const polyhedron_shape &shA, const sphere_shape &shB,
                          const collision_context &ctx) {
     return {};
 }
 
-collision_result collide(const polyhedron_shape &shA, const sphere_shape &shB,
+collision_result collide(const sphere_shape &shA, const polyhedron_shape &shB, 
                          const collision_context &ctx) {
     return swap_collide(shA, shB, ctx);
 }
