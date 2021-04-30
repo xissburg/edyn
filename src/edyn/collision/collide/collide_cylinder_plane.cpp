9ab52e6ff5cf0e9806e9611d1409962b483b7bc8
xissburg <xissburg@xissburg.com>
2021-04-29 19:38:02 -0500

    Cyl-plane collision improvements.

--- src/edyn/collision/collide/collide_cylinder_plane.cpp
+++ src/edyn/collision/collide/collide_cylinder_plane.cpp
@@ -41,32 +41,33 @@ void collide(const cylinder_shape &shA, const plane_shape &shB,
         }
         break;
     }
-    case cylinder_feature::side_edge: {
+    case cylinder_feature::side_edge:
+    case cylinder_feature::cap_edge: {
         auto cyl_axis = quaternion_x(ornA);
-        auto cyl_vertices = std::array<vector3, 2>{
-            posA - cyl_axis * shA.half_length,
-            posA + cyl_axis * shA.half_length
-        };
-        auto dirA = project_direction(-normal, cyl_axis);
+        auto cyl_vertices = std::array<vector3, 2>{};
+        auto num_vertices = 0;
+        
+        if (featureA == cylinder_feature::cap_edge) {
+            cyl_vertices[0] = posA + cyl_axis * shA.half_length * to_sign(feature_indexA == 0);
+            num_vertices = 1;
+        } else {
+            cyl_vertices[0] = posA - cyl_axis * shA.half_length;
+            cyl_vertices[1] = posA + cyl_axis * shA.half_length;
+            num_vertices = 2;
+        }
 
-        for (auto &vertex : cyl_vertices) {
+        // Negated normal projected onto plane of cylinder caps. Multiply it by
+        // radius and add the cap center to get the exact pivot on A.
+        auto dirA = normalize(project_direction(-normal, cyl_axis));
+
+        for (auto i = 0; i < num_vertices; ++i) {
+            auto &vertex = cyl_vertices[i];
             auto pivotA_world = vertex + dirA * shA.radius;
             auto pivotA = to_object_space(pivotA_world, posA, ornA);
             auto pivotB = project_plane(pivotA_world, center, normal);
             auto local_distance = dot(pivotA_world - pivotB, normal);
             result.maybe_add_point({pivotA, pivotB, normal, local_distance});
         }
-        break;
-    }
-    case cylinder_feature::cap_edge: {
-        auto cyl_axis = quaternion_x(ornA);
-        auto vertex = posA + cyl_axis * shA.half_length * to_sign(feature_indexA == 0);
-        auto dirA = project_direction(-normal, cyl_axis);
-        auto pivotA_world = vertex + dirA * shA.radius;
-        auto pivotA = to_object_space(pivotA_world, posA, ornA);
-        auto pivotB = project_plane(pivotA_world, center, normal);
-        auto local_distance = dot(pivotA_world - pivotB, normal);
-        result.maybe_add_point({pivotA, pivotB, normal, local_distance});
         break;
     }
     }
