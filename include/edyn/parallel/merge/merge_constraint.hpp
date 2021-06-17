f3741c6eda19a442ab086f5a7aa11221f49dc606
xissburg <xissburg@xissburg.com>
2021-06-17 01:11:22 -0500

    Merge exh constraints.

--- include/edyn/parallel/merge/merge_constraint.hpp
+++ include/edyn/parallel/merge/merge_constraint.hpp
@@ -54,6 +54,54 @@ void merge(const gravity_constraint *old_comp, gravity_constraint &new_comp, mer
     merge(static_cast<const constraint_base *>(old_comp), static_cast<constraint_base &>(new_comp), ctx);
 }
 
+template<> inline
+void merge(const contact_patch_constraint *old_comp, contact_patch_constraint &new_comp, merge_context &ctx) {
+    merge(static_cast<const constraint_base *>(old_comp), static_cast<constraint_base &>(new_comp), ctx);
+}
+
+template<> inline
+void merge(const antiroll_constraint *old_comp, antiroll_constraint &new_comp, merge_context &ctx) {
+    merge(static_cast<const constraint_base *>(old_comp), static_cast<constraint_base &>(new_comp), ctx);
+    new_comp.third_entity = ctx.map->remloc(new_comp.third_entity);
+}
+
+template<> inline
+void merge(const doublewishbone_constraint *old_comp, doublewishbone_constraint &new_comp, merge_context &ctx) {
+    merge(static_cast<const constraint_base *>(old_comp), static_cast<constraint_base &>(new_comp), ctx);
+}
+
+template<> inline
+void merge(const tierod_constraint *old_comp, tierod_constraint &new_comp, merge_context &ctx) {
+    merge(static_cast<const constraint_base *>(old_comp), static_cast<constraint_base &>(new_comp), ctx);
+}
+
+template<> inline
+void merge(const tirecarcass_constraint *old_comp, tirecarcass_constraint &new_comp, merge_context &ctx) {
+    merge(static_cast<const constraint_base *>(old_comp), static_cast<constraint_base &>(new_comp), ctx);
+}
+
+template<> inline
+void merge(const springdamper_constraint *old_comp, springdamper_constraint &new_comp, merge_context &ctx) {
+    merge(static_cast<const constraint_base *>(old_comp), static_cast<constraint_base &>(new_comp), ctx);
+}
+
+template<> inline
+void merge(const spin_angle_constraint *old_comp, spin_angle_constraint &new_comp, merge_context &ctx) {
+    merge(static_cast<const constraint_base *>(old_comp), static_cast<constraint_base &>(new_comp), ctx);
+}
+
+template<> inline
+void merge(const spin_constraint *old_comp, spin_constraint &new_comp, merge_context &ctx) {
+    merge(static_cast<const constraint_base *>(old_comp), static_cast<constraint_base &>(new_comp), ctx);
+}
+
+template<> inline
+void merge(const differential_constraint *old_comp, differential_constraint &new_comp, merge_context &ctx) {
+    for (auto &entity : new_comp.body) {
+        entity = ctx.map->remloc(entity);
+    }
+}
+
 }
 
 #endif // EDYN_PARALLEL_MERGE_MERGE_CONSTRAINT_HPP
