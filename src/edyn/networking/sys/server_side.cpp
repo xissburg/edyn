fc877e19a2181fef27d9638c6ae919d5b7f29b8d
xissburg <xissburg@xissburg.com>
2023-04-04 09:36:21 -0500

    Do not import inputs in the main registry immediately in async mode or else they might be replaced later with a snapshot coming from the worker with a value that was sent earlier.

--- src/edyn/networking/sys/server_side.cpp
+++ src/edyn/networking/sys/server_side.cpp
@@ -43,15 +43,8 @@
 namespace edyn {
 
 static void process_packet(entt::registry &registry, entt::entity client_entity, packet::registry_snapshot &snapshot) {
-    auto &ctx = registry.ctx().at<server_network_context>();
-    auto &settings = registry.ctx().at<edyn::settings>();
-
-    if (settings.execution_mode == edyn::execution_mode::asynchronous) {
-        // Import inputs directly into the main registry.
-        ctx.snapshot_importer->import_input_local(registry, client_entity, snapshot);
-
-        auto &stepper = registry.ctx().at<stepper_async>();
-        stepper.send_message_to_worker<msg::apply_network_pools>(std::move(snapshot.entities), std::move(snapshot.pools), false);
+    if (auto *stepper = registry.ctx().find<stepper_async>()) {
+        stepper->send_message_to_worker<msg::apply_network_pools>(std::move(snapshot.entities), std::move(snapshot.pools), false);
     } else {
         snap_to_pool_snapshot(registry, snapshot.entities, snapshot.pools, false);
         wake_up_island_residents(registry, snapshot.entities);
