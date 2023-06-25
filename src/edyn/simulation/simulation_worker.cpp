13f3b450f30cca940a2c5f0b7216923a57db6d8b
xissburg <xissburg@xissburg.com>
2023-06-25 12:19:44 -0500

    Scale up the simulation time delta in case the number of steps is above maximum. This effectively slows down the presentation.

--- src/edyn/simulation/simulation_worker.cpp
+++ src/edyn/simulation/simulation_worker.cpp
@@ -315,11 +315,12 @@ void simulation_worker::update() {
     m_accumulated_time -= static_cast<double>(num_steps) * fixed_dt;
 
     auto effective_steps = num_steps;
+    auto sim_dt = fixed_dt;
 
     if (num_steps > settings.max_steps_per_update) {
         effective_steps = settings.max_steps_per_update;
-        // Advance sim time to account for steps skipped.
-        m_sim_time += (num_steps - effective_steps) * fixed_dt;
+        // Adjust sim time to account for steps skipped.
+        sim_dt = (elapsed - m_accumulated_time) / effective_steps;
     }
 
     m_poly_initializer.init_new_shapes();
@@ -338,7 +339,7 @@ void simulation_worker::update() {
         nphase.update(true);
         m_solver.update(true);
 
-        m_sim_time += fixed_dt;
+        m_sim_time += sim_dt;
 
         if (settings.clear_actions_func) {
             (*settings.clear_actions_func)(m_registry);
