167d84f0b687925d4ed9d24493d860adc1b57360
xissburg <xissburg@xissburg.com>
2025-07-18 16:24:46 -0500

    Update calculated components after extrapolation result is merged.

--- src/edyn/networking/util/process_extrapolation_result.cpp
+++ src/edyn/networking/util/process_extrapolation_result.cpp
@@ -3,6 +3,10 @@
 #include "edyn/networking/util/import_contact_manifolds.hpp"
 #include "edyn/networking/sys/accumulate_discontinuities.hpp"
 #include "edyn/networking/sys/assign_previous_transforms.hpp"
+#include "edyn/sys/update_aabbs.hpp"
+#include "edyn/sys/update_inertias.hpp"
+#include "edyn/sys/update_origins.hpp"
+#include "edyn/sys/update_rotated_meshes.hpp"
 #include "edyn/util/island_util.hpp"
 
 namespace edyn {
@@ -17,6 +21,15 @@ entt::sparse_set get_entities_from_extrapolation_result(const extrapolation_resu
     }
 
     return entities;
+}
+
+void post_extrapolation_update(entt::registry &registry, const std::vector<entt::entity> &entities) {
+    update_origins(registry, entities);
+    update_rotated_meshes(registry, entities);
+    update_aabbs(registry, entities);
+    auto island_entities = collect_islands_from_residents(registry, entities);
+    update_island_aabbs(registry, island_entities);
+    update_inertias(registry, entities);
 }
 
 void process_extrapolation_result(entt::registry &registry, entity_map &emap,
@@ -34,6 +47,17 @@ void process_extrapolation_result(entt::registry &registry, entity_map &emap,
     // Wake up affected entities.
     auto remote_entities = get_entities_from_extrapolation_result(result);
     wake_up_island_residents(registry, remote_entities, emap);
+
+    std::vector<entt::entity> local_entities;
+    local_entities.reserve(remote_entities.size());
+
+    for (auto remote_entity : remote_entities) {
+        if (emap.contains(remote_entity)) {
+            local_entities.push_back(emap.at(remote_entity));
+        }
+    }
+
+    post_extrapolation_update(registry, local_entities);
 }
 
 void process_extrapolation_result(entt::registry &registry,
@@ -51,6 +75,10 @@ void process_extrapolation_result(entt::registry &registry,
     // Wake up affected entities.
     auto entities = get_entities_from_extrapolation_result(result);
     wake_up_island_residents(registry, entities);
+
+    std::vector<entt::entity> local_entities;
+    local_entities.insert(local_entities.end(), entities.begin(), entities.end());
+    post_extrapolation_update(registry, local_entities);
 }
 
 }
